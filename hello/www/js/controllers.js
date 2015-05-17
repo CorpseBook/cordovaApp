be705cd644a6b1a355b660a0f31145b7c872144c
Piet Geursen <pietgeursen@gmail.com>
2015-05-17 18:42:46 +1200

    MVP when served on the browser. Android location doesn't work

--- hello/www/js/controllers.js
+++ hello/www/js/controllers.js
@@ -1,4 +1,5 @@
-
+var url = 'https://corpsebook-server.herokuapp.com/'
+// var url = 'http://192.168.0.2:3000/'
 
 corpseFaceApp.controller('contributionNewCtrl', ['$scope', '$http', '$routeParams', '$location', 'getStory',
   function ($scope, $http, $routeParams, $location, getStory) {
@@ -14,6 +15,40 @@ corpseFaceApp.controller('contributionNewCtrl', ['$scope', '$http', '$routeParam
         console.log('Got error trying to get story: ', error)
     })
 
+    var getInrange = function ()
+    {
+      var data = {search: {lat: $scope.lat, lng: $scope.lng}};
+
+      var config =
+      {
+        method: 'POST',
+        url: url + 'stories/' + $routeParams.id + '/in_range',
+        data: data,
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        }
+      };
+      $http(config)
+        .success(function (data)
+        {
+          $scope.story.in_range = data.in_range;
+        })
+        .error(function (data, status)
+        {
+          console.log(status, data);
+        });
+    }
+
+    navigator.geolocation.getCurrentPosition(function(data){
+      console.log("Got position: ", data);
+      $scope.lat = data.coords.latitude
+      $scope.lng = data.coords.longitude
+
+      getInrange();
+    });
+
+
     $scope.createContribution = function(contribution)
     {
       contribution = {contribution : contribution}
@@ -21,7 +56,7 @@ corpseFaceApp.controller('contributionNewCtrl', ['$scope', '$http', '$routeParam
       var config =
       {
         method: 'POST',
-        url: 'https://corpsebook-server.herokuapp.com/stories/'+ $routeParams.id +'/contributions',
+        url: url + 'stories/'+ $routeParams.id +'/contributions',
         data: contribution
       };
 
@@ -61,7 +96,7 @@ corpseFaceApp.controller('storiesNewCtrl', ['$scope', '$http',
       var config =
       {
         method: 'POST',
-        url: 'https://corpsebook-server.herokuapp.com/stories',
+        url: url + 'stories',
         data: story
       };
 
@@ -103,7 +138,7 @@ corpseFaceApp.controller('storiesCtrl', ['$scope', '$http', '$location',
       var config =
       {
         method: 'GET',
-        url: 'https://corpsebook-server.herokuapp.com/stories',
+        url: url + 'stories',
       };
       $http(config)
       .success(function (data)
